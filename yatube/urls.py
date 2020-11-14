1f2bd497535905033c8cbf10cf4270c1fa4e9652
Morozov Alexander <morozov_a@protei.ru>
2020-11-14 22:16:11 +0300

    Refactorint and add new urls

--- yatube/urls.py
+++ yatube/urls.py
@@ -9,42 +9,38 @@ handler404 = "posts.views.page_not_found"
 handler500 = "posts.views.server_error"
 
 urlpatterns = [
-     path('about-author/',
-          views.flatpage,
-          {'url': '/about-author/'},
-          name='about-author'),
-     path('about-spec/',
-          views.flatpage,
-          {'url': '/about-spec/'},
-          name='about-spec'),
-     path('about-us/',
-          views.flatpage,
-          {'url': '/about-us/'},
-          name='about-us'),
-     path('admin/',
-          admin.site.urls),
-     path('auth/',
-          include('users.urls')),
-     path('auth/',
-          include('django.contrib.auth.urls')),
-     path('contacts/',
-          views.flatpage,
-          {'url': '/contacts/'},
-          name='contacts'),
-     path('terms/',
-          views.flatpage,
-          {'url': '/terms/'},
-          name='terms'),
-     path('',
-          include('posts.urls')),
+    path('about-author/',
+         views.flatpage,
+         {'url': '/about-author/'},
+         name='about-author'),
+    path('about-spec/',
+         views.flatpage,
+         {'url': '/about-spec/'},
+         name='about-spec'),
+    path('about-us/',
+         views.flatpage,
+         {'url': '/about-us/'},
+         name='about-us'),
+    path('admin/',
+         admin.site.urls),
+    path('auth/',
+         include('users.urls')),
+    path('auth/',
+         include('django.contrib.auth.urls')),
+    path('contacts/',
+         views.flatpage,
+         {'url': '/contacts/'},
+         name='contacts'),
+    path('terms/',
+         views.flatpage,
+         {'url': '/terms/'},
+         name='terms'),
+    path('',
+         include('posts.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-         settings.MEDIA_URL,
-         document_root=settings.MEDIA_ROOT
-     )
-    urlpatterns += static(
-         settings.STATIC_URL,
-         document_root=settings.STATIC_ROOT
-     )
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
